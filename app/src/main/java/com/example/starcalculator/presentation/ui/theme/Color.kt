93587e9551a78a7bbfd4235712a35d2ef1a05f79
alperen-sakin <azapkaan@gmail.com>
2026-05-10 01:34:09 +0200

    Add new color constants and TitleGradient to theme

--- app/src/main/java/com/example/starcalculator/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/example/starcalculator/presentation/ui/theme/Color.kt
@@ -9,3 +9,28 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val SlateGray300 = Color(0xFFcbd5e1)
+val SlateGray400 = Color(0xFF94a3b8)
+val SlateGray500 = Color(0xFF64748b)
+val SlateGray600 = Color(0xFF475569)
+val SlateGray700 = Color(0xFF334155)
+val SlateGray800 = Color(0xFF1e293b)
+val SlateGray900 = Color(0xFF0f172a)
+val SlateGray950 = Color(0xFF020617)
+
+val Yellow400 = Color(0xFFfacc15)
+val Yellow500 = Color(0xFFeab308)
+val Yellow900 = Color(0xFF713f12)
+
+val Amber600 = Color(0xFFd97706)
+
+val Emerald400 = Color(0xFF34d399)
+val Emerald500 = Color(0xFF10b981)
+val Emerald600 = Color(0xFF059669)
+
+val Red500 = Color(0xFFef4444)
+
+val Cyan400 = Color(0xFF22d3ee)
+
+val TitleGradient = listOf(Yellow400, Amber600)
