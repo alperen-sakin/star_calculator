45074cee36cafbe239e4d90b85d3ad2eb7dbbb2d
alperen-sakin <azapkaan@gmail.com>
2026-05-10 03:24:58 +0200

    Add CustomTopAppBar component

--- app/src/main/java/com/example/starcalculator/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/starcalculator/presentation/home/HomeScreen.kt
@@ -1,14 +1,33 @@
 package com.example.starcalculator.presentation.home
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.starcalculator.presentation.home.component.CustomTopAppBar
+import com.example.starcalculator.presentation.ui.theme.SlateGray900
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
-
-    Text(
-        text = "Home Screen",
-        modifier = modifier
-    )
+    Scaffold(
+        topBar = {
+            Column {
+                CustomTopAppBar(
+                    onResetClick = {}
+                )
+                HorizontalDivider(
+                    thickness = 2.dp,
+                    color = SlateGray900
+                )
+            }
+        }
+    ) {
+        Column(
+            modifier = modifier
+                .padding(paddingValues = it)
+        ) { }
+    }
 }
