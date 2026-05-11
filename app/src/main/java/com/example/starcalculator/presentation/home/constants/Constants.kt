47fa79e4db6691eee954cd986285af69fb3a716a
alperen-sakin <azapkaan@gmail.com>
2026-05-11 17:44:48 +0200

    Add Constants object for home screen

---
+++ app/src/main/java/com/example/starcalculator/presentation/home/constants/Constants.kt
@@ -0,0 +1,12 @@
+package com.example.starcalculator.presentation.home.constants
+
+object Constants {
+
+    const val MAX_STARS = 10
+    const val MAX_LEVEL_LENGTH = 5
+
+    private const val TWO = 2
+    private const val THREE = 3
+
+    val ROWS = listOf(TWO, THREE, THREE, TWO)
+}
