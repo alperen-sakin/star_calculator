96daa55eb4fa761498c6b1df3556d143f8ee034a
alperen-sakin <azapkaan@gmail.com>
2026-05-16 05:59:24 +0200

    Update AppModule to provide CalculateTotalCostUseCase

--- app/src/main/java/com/example/starcalculator/di/AppModule.kt
+++ app/src/main/java/com/example/starcalculator/di/AppModule.kt
@@ -1,9 +1,18 @@
 package com.example.starcalculator.di
 
+import com.example.starcalculator.domain.useCase.CalculateTotalCostUseCase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object AppModule
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideCalculateTotalCostUseCase(): CalculateTotalCostUseCase {
+        return CalculateTotalCostUseCase()
+    }
+}
