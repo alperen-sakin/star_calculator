d3c681e3aab8490dfa525fd51a1374f78d8241d0
alperen-sakin <azapkaan@gmail.com>
2026-05-16 05:10:25 +0200

    Add CustomFloatingButton and BottomSection to HomeScreen

--- app/src/main/java/com/example/starcalculator/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/starcalculator/presentation/home/HomeScreen.kt
@@ -6,10 +6,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.FabPosition
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
@@ -17,6 +19,7 @@ import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.starcalculator.presentation.home.component.CustomFloatingButton
 import com.example.starcalculator.presentation.home.component.CustomTopAppBar
 import com.example.starcalculator.presentation.home.component.SetAllStarsBox
 import com.example.starcalculator.presentation.home.component.parametersSection.ParameterSectionEvents
@@ -29,6 +32,7 @@ import com.example.starcalculator.presentation.home.viewmodel.HomeState
 import com.example.starcalculator.presentation.home.viewmodel.HomeViewModel
 import com.example.starcalculator.presentation.home.viewmodel.StarsStates
 import com.example.starcalculator.presentation.ui.theme.SlateGray900
+import kotlinx.coroutines.launch
 
 @Composable
 fun HomeScreen(
@@ -37,6 +41,9 @@ fun HomeScreen(
 ) {
     val state = viewModel.state.collectAsStateWithLifecycle()
     val starsStates = viewModel.starsStates.collectAsStateWithLifecycle()
+
+    val coroutineScope = rememberCoroutineScope()
+    val scrollState = rememberScrollState()
 
     val focusManager = LocalFocusManager.current
 
@@ -57,46 +64,66 @@ fun HomeScreen(
                     color = SlateGray900
                 )
             }
+        },
+        floatingActionButtonPosition = FabPosition.Center,
+        floatingActionButton = {
+            CustomFloatingButton(
+                onCalculateClick = {
+                    focusManager.clearFocus()
+
+                    coroutineScope.launch {
+                        scrollState.animateScrollTo(scrollState.maxValue)
+                    }
+                }
+            )
         }
     ) {
         Column(
             modifier = modifier
                 .padding(paddingValues = it)
-                .padding(vertical = 16.dp, horizontal = 8.dp)
-                .verticalScroll(rememberScrollState()),
+                .padding(horizontal = 8.dp)
+                .verticalScroll(scrollState),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
             TopSection(viewModel, state, starsStates)
 
-            ParametersSection(
-                state = ParameterSectionState(
-                    achievementLvl2 = state.value.achievementLvl2,
-                    masteryLvl17 = state.value.masteryLvl17,
-                    scrapyardV2 = state.value.scrapyardV2,
-                    targetStar = state.value.targetStar,
-                ),
-
-                events = ParameterSectionEvents(
-                    onAchievementLvl2Change = viewModel::onAchievementLvl2Change,
-                    onMasteryLvl17Change = viewModel::onMasteryLvl17Change,
-                    onScrapyardV2Change = viewModel::onScrapyardV2Change,
-                    onTargetStarChange = viewModel::onTargetStarChange,
-                    onAdjustmentClick = viewModel::onAdjustmentClick
-
-                )
-
-            )
-
-            ResultSection(
-                state = ResultSectionState(
-                    costMagnet = state.value.costMagnet,
-                    costGoldenScrap = state.value.costGoldenScrap,
-                    costFragment = state.value.costFragment
-                )
-            )
+            BottomSection(state, viewModel)
         }
     }
+}
+
+@Composable
+private fun BottomSection(
+    state: State<HomeState>,
+    viewModel: HomeViewModel
+) {
+    ParametersSection(
+        state = ParameterSectionState(
+            achievementLvl2 = state.value.achievementLvl2,
+            masteryLvl17 = state.value.masteryLvl17,
+            scrapyardV2 = state.value.scrapyardV2,
+            targetStar = state.value.targetStar,
+        ),
+
+        events = ParameterSectionEvents(
+            onAchievementLvl2Change = viewModel::onAchievementLvl2Change,
+            onMasteryLvl17Change = viewModel::onMasteryLvl17Change,
+            onScrapyardV2Change = viewModel::onScrapyardV2Change,
+            onTargetStarChange = viewModel::onTargetStarChange,
+            onAdjustmentClick = viewModel::onAdjustmentClick
+
+        )
+
+    )
+
+    ResultSection(
+        state = ResultSectionState(
+            costMagnet = state.value.costMagnet,
+            costGoldenScrap = state.value.costGoldenScrap,
+            costFragment = state.value.costFragment
+        )
+    )
 }
 
 @Composable
@@ -110,6 +137,7 @@ private fun TopSection(
             viewModel.onAllLevelChange(allLevel)
         },
         allLevel = state.value.allLevel,
+        modifier = Modifier.padding(top = 16.dp)
     )
 
     StarsSection(
